ccdf7a426ecc247f72b656538cdf45a7919a4d4d
Tim Mutton <tim.mutton@outware.com.au>
2016-03-02 08:35:48 +1100

    Revert "Use UiModeManager to add Marshmallow support"
    This reverts commit 2ec1563b6e63332ea07b3cdea3dff8294a8112e9.

--- app/src/main/java/au/com/timmutton/daynightvectors/DayNightApplication.java
+++ app/src/main/java/au/com/timmutton/daynightvectors/DayNightApplication.java
@@ -1,8 +1,6 @@
 package au.com.timmutton.daynightvectors;
 
 import android.app.Application;
-import android.app.UiModeManager;
-import android.content.Context;
 import android.support.v7.app.AppCompatDelegate;
 
 /**
@@ -11,14 +9,5 @@ import android.support.v7.app.AppCompatDelegate;
 public class DayNightApplication extends Application {
     static {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        // add this code for 6.0
-        UiModeManager uiManager = (UiModeManager) getSystemService(Context.UI_MODE_SERVICE);
-        uiManager.setNightMode(UiModeManager.MODE_NIGHT_AUTO);
     }
 }
