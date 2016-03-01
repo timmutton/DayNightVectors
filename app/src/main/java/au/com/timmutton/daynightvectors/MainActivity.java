2ec1563b6e63332ea07b3cdea3dff8294a8112e9
Tim Mutton <tim.mutton@outware.com.au>
2016-03-01 13:36:02 +1100

    Use UiModeManager to add Marshmallow support

--- app/src/main/java/au/com/timmutton/daynightvectors/MainActivity.java
+++ app/src/main/java/au/com/timmutton/daynightvectors/MainActivity.java
@@ -1,5 +1,7 @@
 package au.com.timmutton.daynightvectors;
 
+import android.app.UiModeManager;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -16,12 +18,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final UiModeManager uiManager = (UiModeManager) getSystemService(Context.UI_MODE_SERVICE);
         Switch dayNightToggle = (Switch) findViewById(R.id.toggle_day_night);
         dayNightToggle.setChecked((getResources().getConfiguration().uiMode
                 & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES);
         dayNightToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                uiManager.setNightMode(isChecked ? UiModeManager.MODE_NIGHT_YES : UiModeManager.MODE_NIGHT_NO);
                 getDelegate().setLocalNightMode(isChecked ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO);
                 getDelegate().applyDayNight();
                 recreate();
