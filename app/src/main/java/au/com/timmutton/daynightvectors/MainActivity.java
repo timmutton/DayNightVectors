6c8ac0b3d5af64909c5cce1f050d356fe8e0da7b
Tim Mutton <tim.mutton@outware.com.au>
2016-04-11 11:47:52 +1000

    Update support library version to add support for marshmallow devices, add workaround for bug on marshmallow devices

--- app/src/main/java/au/com/timmutton/daynightvectors/MainActivity.java
+++ app/src/main/java/au/com/timmutton/daynightvectors/MainActivity.java
@@ -1,6 +1,7 @@
 package au.com.timmutton.daynightvectors;
 
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
@@ -16,14 +17,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final int currentNightMode = getResources().getConfiguration().uiMode
+                & Configuration.UI_MODE_NIGHT_MASK;
+
         Switch dayNightToggle = (Switch) findViewById(R.id.toggle_day_night);
-        dayNightToggle.setChecked((getResources().getConfiguration().uiMode
-                & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES);
+        dayNightToggle.setChecked(currentNightMode == Configuration.UI_MODE_NIGHT_YES);
         dayNightToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 getDelegate().setLocalNightMode(isChecked ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO);
                 getDelegate().applyDayNight();
+                if(Build.VERSION.SDK_INT == Build.VERSION_CODES.M && currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+                    Configuration config = getResources().getConfiguration();
+                    config.uiMode = (config.uiMode & ~Configuration.UI_MODE_NIGHT_MASK) | Configuration.UI_MODE_NIGHT_YES;
+                    getResources().updateConfiguration(config, null);
+                }
                 recreate();
             }
         });
